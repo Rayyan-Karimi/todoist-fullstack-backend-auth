f909f05db16763f5f984b30369e27f06819fc825
Rayyan-Karimi <rayyan.karimi.30.3@mountblue.tech>
2024-12-13 16:51:11 +0530

    Changes started for promises from callbacks.

--- models/tasks.model.js
+++ models/tasks.model.js
@@ -10,37 +10,30 @@ class Task {
         this.created_at = created_at;
     }
 
-    static create(newTask, response) {
-        const query = `
-        INSERT INTO tasks (content, description, due_date, is_completed, created_at, project_id)
-        VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?)
-        `;
-        const params = [newTask.content, newTask.description, newTask.due_date, newTask.is_completed, newTask.project_id]
-        db.run(query, params, function (err) {
-            if (err) {
-                console.error("Error creating new Task", err.message)
-                response(err, null)
-            } else {
-                console.log("Created new Task successfully!", { id: this.lastID, ...newTask })
-                response(null, { id: this.lastID, ...newTask })
-            }
+    static create(newTask) {
+        return new Promise ((resolve, reject) => {
+            const query = `
+            INSERT INTO tasks (content, description, due_date, is_completed, created_at, project_id)
+            VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?)
+            `;
+            const params = [newTask.content, newTask.description, newTask.due_date, newTask.is_completed, newTask.project_id]
+            db.run(query, params, function (err) {
+                if (err) reject(err);
+                else resolve({ id: this.lastID, ...newTask });
+            })
         })
     }
 
-    static findAll(taskId, response) {
-        let query = "Select * from tasks"
-        if (taskId) query += ` Where id = ${taskId}`
-        db.all(query, (err, rows) => {
-            if (err) {
-                const errorMessage = taskId ? `Error retrieving task with ID ${taskId}` : "Error retrieving all tasks";
-                console.error(errorMessage, err.message);
-                response(err, null);
-            } else {
-                const successMessage = taskId ? `Task with ID ${taskId} retrieved successfully!` : "All tasks retrieved successfully!";
-                console.log(successMessage);
-                response(null, rows);
-            }
+    static findAll(taskId) {
+        return new Promise ((resolve, reject) => {
+            let query = "Select * from tasks"
+            if (taskId) query += ` Where id = ${taskId}`
+            db.all(query, (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            })
         })
+        
     }
 
     static filter = ({projectId, dueDate, isCompleted, createdAt}) => {
@@ -70,26 +63,27 @@ class Task {
         })
     }
 
-    static update(taskId, updatedTask, response) {
-        const query = `
-        UPDATE tasks
-        SET content = ?, description = ?, due_date = ?, is_completed = ?, project_id = ?
-        WHERE id = ?
-        `;
-        const params = [updatedTask.content, updatedTask.description, updatedTask.due_date, updatedTask.is_completed, updatedTask.project_id, taskId];
+    static update(taskId, updatedTask) {
+        return new Promise ((resolve, reject) => {
+            const query = `
+            UPDATE tasks
+            SET content = ?, description = ?, due_date = ?, is_completed = ?, project_id = ?
+            WHERE id = ?
+            `;
+            const params = [updatedTask.content, updatedTask.description, updatedTask.due_date, updatedTask.is_completed, updatedTask.project_id, taskId];            
+            db.run(query, params, function (err) {
+                if (err) {
+                    reject(err);
+                } else if (this.changes === 0) {
+                    resolve({ message: `No task found with id=${taskId}` });
+                } else {
+                    resolve({ id: taskId, ...updatedTask });
+                }
+            });
+        })
+        
 
-        db.run(query, params, function (err) {
-            if (err) {
-                console.error("Error updating the Task with task id =", taskId, err.message);
-                response(err, null);
-            } else if (this.changes === 0) {
-                console.log("No task found with id =", taskId);
-                response({ message: `No task found with id=${taskId}` }, null);
-            } else {
-                console.log("Updated task with id =", taskId, updatedTask);
-                response(null, { id: taskId, ...updatedTask });
-            }
-        });
+        
     }
 
     static remove(taskId, response) {

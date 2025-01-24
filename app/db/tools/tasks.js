e9b125103c67eecff6ab9ce386a74a18cf999524
rayyan6 <rayyan.karimi.30.3@mountblue.tech>
2025-01-24 17:01:26 +0530

    Whatta projectgit add .

--- app/db/tools/tasks.js
+++ app/db/tools/tasks.js
@@ -7,7 +7,7 @@ export default function generateTasks(num, maxProjects) {
             content: faker.lorem.sentence(),
             description: faker.lorem.paragraph(),
             due_date: faker.date.future().toISOString().split('T')[0],
-            is_completed: faker.datatype.boolean(),
+            is_completed: false,
             project_id: faker.number.int({ min: 1, max: maxProjects })
         });
         console.log(`Generated ${i} Task`)
